adbc57608ac67d6485f9255eb70a053bb1726e54
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-06-26 20:57:55 +0530

    Update main.tf

--- s3backend/main.tf
+++ s3backend/main.tf
@@ -21,7 +21,7 @@ module "ec2_instance" {
 }
 terraform {
   backend "s3" {
-    bucket         = "rudram" # Replace with your S3 bucket name
+    bucket         = "mybucketaws432" # Replace with your S3 bucket name
     key            = "terraform.tfstate" # Key is the name of the state file in the bucket
     region         = "us-east-1" # Replace with your desired AWS region
     encrypt        = true
