770192d55a4e36823fe91e8e71538f73ea1452fb
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-09-12 21:26:35 +0530

    Update main.tf

--- s3backend/main.tf
+++ s3backend/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "us-east-1"
 }
 
 module "ec2_instance" {
@@ -8,9 +8,9 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-04f8d7ed2f1a54b14"
+  ami                    = "ami-0182f373e66f89c85"
   instance_type          = "t2.micro"
-  key_name               = "testdd"
+  key_name               = "vpc"
   monitoring             = true
   
   tags = {
@@ -21,9 +21,9 @@ module "ec2_instance" {
 }
 terraform {
   backend "s3" {
-    bucket         = "rudram" # Replace with your S3 bucket name
+    bucket         = "rajutraingingtest" # Replace with your S3 bucket name
     key            = "terraform.tfstate" # Key is the name of the state file in the bucket
-    region         = "ap-south-1" # Replace with your desired AWS region
+    region         = "us-east-1" # Replace with your desired AWS region
     encrypt        = true
   }
 }
