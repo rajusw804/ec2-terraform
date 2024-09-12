5d28fcd5a380f23313799b821cdb863b5e6eb550
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-09-12 21:31:21 +0530

    Create main.tf

---
+++ single/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "xxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxx"
+}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
+  key_name               = "10am"
+  monitoring             = true
+  
+  tags = {
+    Name = "terraform-instance"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
