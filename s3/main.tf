742dbad7d05b34a1859b00cc2f9926e67919e19c
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-12-28 21:52:02 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-avan-name"  # Specify a globally unique bucket name
 
-  acl    = "public-read"  # Access Control List for the bucket, can be adjusted based on your needs
+  acl    = "private"  # Access Control List for the bucket, can be adjusted based on your needs
 
   tags = {
     Name        = "my-unique-avan-name"
