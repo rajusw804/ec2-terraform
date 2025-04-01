feca5babb9542ffb864dd09e20da3f7f13b5eedc
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2025-04-01 08:21:08 +0530

    Update main.tf

--- multiple/main.tf
+++ multiple/main.tf
@@ -9,11 +9,11 @@ module "ec2_instance" {
 
   name = "instance-${each.key}"
 
-  ami                    = "ami-012967cc5a8c9f891"
+  ami                    = "ami-071226ecf16aa7d96"
   instance_type          = "t2.micro"
-  key_name               = "vpc"
+  key_name               = "tets22"
   monitoring             = true
-  vpc_security_group_ids = ["sg-088ecb59a8078e11c"]
+  vpc_security_group_ids = ["sg-0c7d4230ac5a068f8"]
   subnet_id              = "subnet-0051dcb35a8f998ca"
 
   tags = {
