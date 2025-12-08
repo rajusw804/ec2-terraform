3ecd077556f3373086b1eab1de118fb81ea4864d
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2025-12-08 08:39:40 +0530

    Update main.tf

--- datasource/main.tf
+++ datasource/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "ap-southeast-2" # Or your desired region
+  region = "us-east-1" # Or your desired region
 }
 
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t3.micro"
+  key_name = "rajavan"
   tags = {
     Name = "My-App-Server"
   }
