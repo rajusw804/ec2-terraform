2fd60bd398aee08c3fcfeeee5cbc0f1bb66a468d
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2026-02-13 07:51:46 +0530

    Update main.tf

--- basic/main.tf
+++ basic/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "ap-southeast-2" # Or your desired region
+  region = "us-east-1" # Or your desired region
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0a0b0b06dd1636865"
+  ami           = "ami-0c1fe732b5494dc14"
   instance_type = "t3.micro"
-  key_name = "rajdevops"
+  key_name = "rajavan"
 
   tags = {
-    Name = "MyTerraformEC2Instance"
+    Name = "myfirstinstance"
   }
 }
