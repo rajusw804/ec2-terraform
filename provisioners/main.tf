9336080335b016a52c48b59432d3381993ab27fa
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2026-02-17 07:58:40 +0530

    Update main.tf

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"] # In production, change this to your IP!
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
